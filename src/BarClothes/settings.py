7381961e4f8223c2f537d027e8d2c04a36cb814c
Macbook <macbook@MacBook-Pro-khxng-Macbook.local>
2017-09-26 22:54:28 +0700

    add ui login customer/seller

--- src/BarClothes/settings.py
+++ src/BarClothes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+   
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 if DEBUG:
     STATICFILES_DIRS=(
