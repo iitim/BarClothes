b09077fa2deeb668757ead228a2ea8bd45abff58
Natnaree Asavaseri <natnaree.as@ku.th>
2017-09-26 11:39:10 +0700

    edit url

--- src/BarClothes/urls.py
+++ src/BarClothes/urls.py
@@ -13,20 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from main import views
+# from profile import views as profiles_views
+# from about import views as about_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='home'),
-    # url(r'^login/', views.login, name='login'),
-    # url(r'^logout/', views.logout, name='logout'),
-    # url(r'^about/', views.about, name='about'),
-    # url(r'^about/', views.about, name='about'),
-    # url(r'^about/', views.about, name='about'),
+    # url(r'^$', views.home, name='home'),
+    # url(r'^login/$', views.login, name='login'),
+    # url(r'^logout/$', views.logout, name='logout'),
+    # url(r'^about/$', about_views.about, name='about'),
+    # url(r'^about/$', about_views.about, name='about'),
     # url(r'^profile/$', profiles_views.userProfile, name='profile'),
-    # url(r'^accounts/', include('allauth.urls')),   
+    # url(r'^accounts/$', include('allauth.urls')),   
 
 
 
