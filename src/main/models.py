c9079abbb9c2b24e59746d503e1442f72eb41899
Pawissanutt <pawissanutt@gmail.com>
2017-09-26 18:10:58 +0700

    change model.py

--- src/main/models.py
+++ src/main/models.py
@@ -1,32 +1,25 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
-class Customer(models.Model):
-    username = models.CharField(max_length=45)
-    password = models.CharField(max_length=45)
-    regisDate = models.DateTimeField(default=datetime.now, blank=True)
-    email = models.CharField(max_length=45)
+class UserExtendData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=45) # Customer or Seller
     address = models.CharField(max_length=45)
-    telNo = models.CharField(max_length=45)
+    tel_no = models.CharField(max_length=45)
 
-
-class Seller(models.Model):
-    username = models.CharField(max_length=45)
-    password = models.CharField(max_length=45)
-    regisDate = models.DateTimeField(default=datetime.now, blank=True)
-    email = models.CharField(max_length=45)
-    address = models.CharField(max_length=100)
-    telNo = models.CharField(max_length=45)
-
+# currently hasn't data to be extended
+class SellerExtendData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class Product(models.Model):
     name = models.CharField(max_length=45)
     type = models.CharField(max_length=45)
-    createDate = models.DateTimeField(default=datetime.now, blank=True)
+    create_date = models.DateTimeField(default=datetime.now, blank=True)
     price = models.FloatField()
     detail = models.CharField(max_length=500)
     amount = models.IntegerField()
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    seller = models.ForeignKey(SellerExtendData, on_delete=models.CASCADE)
 
 class Tag(models.Model):
     name = models.CharField(max_length=45)
@@ -34,3 +27,12 @@ class Tag(models.Model):
 class ProductToTag(models.Model):
     product =  models.ForeignKey(Product, on_delete=models.CASCADE)
     tag =  models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+class WaitForBillingProduct(models.Model):
+    customer =  models.ForeignKey(UserExtendData, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    total_price = models.FloatField()
+    status = models.CharField(max_length=45)
+    selected_date = models.DateTimeField(default=datetime.now, blank=True)
+    expire_date = models.DateTimeField(default=datetime.now, blank=True)
