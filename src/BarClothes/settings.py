dd2c2199bb3668a1d0510020296b116921a38160
chayanin <chayanin2007@gmail.com>
2017-09-26 10:34:10 +0630

    fix static problem

--- src/BarClothes/settings.py
+++ src/BarClothes/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if DEBUG:
+    STATICFILES_DIRS=(
+        os.path.join(os.path.dirname(BASE_DIR),"static","static"),
+    )
