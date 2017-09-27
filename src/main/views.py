2a0539b9e3a2980ff862c459f7dc293b32ee9bb2
Peem <4astralp@gmail.com>
2017-09-27 14:59:39 +0700

    edit views.py

--- src/main/views.py
+++ src/main/views.py
@@ -4,3 +4,8 @@ def home(request):
     context = locals()
     template = 'home.html'
     return render(request, template, context)
+
+def catalog(request):
+    context = locals()
+    template = 'catalog.html'
+    return render(request, template, context)
