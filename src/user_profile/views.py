681e8387713b9aa0af4871a1d8b8c0f1a3a9fcfc
ammyblabla <you@example.com>
2017-09-28 21:22:51 +0700

    comment profile/views.py

--- src/user_profile/views.py
+++ src/user_profile/views.py
@@ -7,40 +7,27 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from main.models import UserExtendData 
 # Create your views here.
-# user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
-# user.last_name = 'Lennon'
 
-# class UserExtendData(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     type =  models.CharField(max_length=1, choices=USER_TYPE_CHOICES)
-#     id_num = models.CharField(max_length=13)
-#     address = models.CharField(max_length=100)
-#     tel_no = models.CharField(max_length=45)
+context = {
+    'id_num': 'id',
+    'tel_no': '098765',
+    'address': 'UserExtendData.address',
+}
 
-# context = {
-#     'id_num': 'id',
-#     'tel_no': '098765',
-#     'address': 'UserExtendData.address',
-# }
-
+# uncomment for database testing
 # context = {
 #     'id_num': UserExtendData.id_num,
 #     'tel_no': UserExtendData.tel_no,
 #     'address': UserExtendData.address,
 # # }
+
+# uncomment 1 line below for user profile login test
 # @login_required
 def profile(request):
     # context คือค่าที่ใช้ในการแสดงผลของ template
+    # uncomment 2 lines below for user profile login test
     # user = request.user,
     # context.update({'user':user})
-    # login(request, user)
-    context = {
-        # 'user':user,
-        'id_num': 'id',
-        'tel_no': '098765',
-        'address': 'UserExtendData.address',
-    }
-    print context['tel_no']
     template = 'profile.html'
     return render(request, template, context)
 
