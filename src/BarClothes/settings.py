af4ad723f3b9b3e6e9fc8ccbf585f7196db6d5db
JewJew Thanawat <g-o.v-nus@hotmail.com>
2017-09-30 23:35:37 +0700

    fix media path
    use static/media/product_pic/

--- src/BarClothes/settings.py
+++ src/BarClothes/settings.py
@@ -126,8 +126,5 @@ if DEBUG:
     STATICFILES_DIRS=(
         os.path.join(os.path.dirname(BASE_DIR),"static","static"),
     )
-    '''MEDIAFILES_DIRES=(
-        os.path.join(os.path.dirname(BASE_DIR), "static", "media"),
-    )'''
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(os.path.join(os.path.dirname(BASE_DIR),"static"), 'media')
