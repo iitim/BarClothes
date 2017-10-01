ab21ff44217a80eb02e7a50277c47475e2c693c2
Chayanin <chayanin2007@gmail.com>
2017-10-01 18:09:43 +0700

    refactor app profile

--- src/user_profile/urls.py
+++ src/user_profile/urls.py
@@ -4,13 +4,13 @@ from . import views
 app_name = 'user_profile'
 
 urlpatterns = [
-    url(r'^change-password/success$', views.success, name='profile'),
+    url(r'^change-password/success$', views.success),
     # url(r'^profile$', views.profile, name='profile'),
     url(r'^$', views.profile, name='profile'),
     url(r'^success$', views.success, name='success'),
     url(r'^edit$', views.profile_edit, name='profile_edit'),
     url(r'^change-password/$', views.change_password, name='change_password'),
-    url(r'^change-password/profile$', views.profile, name='profile'),
+    url(r'^change-password/profile$', views.profile),
     url(r'^cancel$', views.cancel, name='cancel'),
      url(r'^profile/(?P<pk>\d+)/$', views.view_profile, name='view_profile_with_pk')
 ]
