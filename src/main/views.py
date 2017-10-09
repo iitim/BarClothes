2b77ffe167ab84109fba2ac099592c8331d86443
bbgun1996 <tanaporn.tha@ku.th>
2017-10-09 22:09:45 +0700

    [home] แก้บัคที่ต้องมี product >= 9 + แก้ css เล็กน้อย

--- src/main/views.py
+++ src/main/views.py
@@ -1,16 +1,24 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, PRODUCT_TYPE_CHOICES
 
 # from main.forms import SignUpForm
 # from main.models import UserExtendData
 
 def home(request):
-    interesting = Product.objects.order_by('pk')[0:9]
-    bestsell = Product.objects.order_by('-pk')[0:9]
+    interesting = Product.objects.order_by('-pk')
+    bestsell = Product.objects.order_by('pk')
+    num_product_interesting = len(interesting)
+    num_product = len(bestsell)
     template = 'home.html'
-    return render(request, template, {'interesting' : interesting, 'bestsell' : bestsell})
+    context = {
+        'interesting' : interesting,
+        'bestsell' : bestsell,
+        'num_product' : num_product,
+        'num_product_interesting' : num_product_interesting
+    }
+    return render(request, template, context)
 
 def store(request):
     context = locals()
