05e94d0f8caf4865b006220c3bf56e51351c50d2
JewJew Thanawat <g-o.v-nus@hotmail.com>
2017-10-09 23:49:52 +0700

    add search algorithm
    edit searchbar (still draft)
    add algorithm version 1 (should inspect & check performance)

--- src/catalog/views.py
+++ src/catalog/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render , redirect
-from .models import Product
+from django.contrib.auth.models import User
+from .models import Product , UserExtendData
 
 def home(request):
     context = locals()
     template = 'home.html'
     return render(request, template, context)
 
-def filter(product_type, num):
+def filter(request, product_type, num):
     page = int(num)
     if page < 1 :
         return -1
     first_product = 18*(page-1)
     last_product = 18*page
     is_last_page = False
-    product = Product.objects.filter(type=product_type).order_by('-create_date')[first_product:last_product]
-    if Product.objects.filter(type=product_type).order_by('-create_date')[last_product:last_product+1] == None :
+    if request.method == 'POST':
+        user_list = User.objects.filter(username__icontains=request.POST['seller_name'])
+        user_extend_data_list = UserExtendData.objects.filter(user__in=user_list)
+        if request.POST['sort'] == 'late' :
+            product = Product.objects.order_by('-create_date')
+        elif request.POST['sort'] == 'old' :
+            product = Product.objects.order_by('create_date')
+        elif request.POST['sort'] == 'low':
+            product = Product.objects.order_by('price')
+        else :
+            product = Product.objects.order_by('-price')
+        product = product.filter(type=product_type, seller__in=user_extend_data_list, name__icontains=request.POST['product_name'])[first_product:last_product+1]
+    else :
+        product = Product.objects.order_by('-create_date').filter(type=product_type)[first_product:last_product+1]
+    if product[last_product:last_product+1] == None :
         is_last_page = True
+    product = product[first_product:last_product]
     context = {
         'type': '/' + product_type,
         'previous_page': page-1,
@@ -26,18 +41,29 @@ def filter(product_type, num):
     return context
 
 def catalog(request, num="1"):
-    print(request.POST)
-    if request.method == 'POST' :
-        pass
     page = int(num)
     if page < 1 :
         return redirect('/shop')
     first_product = 18 * (page - 1)
     last_product = 18*page
     is_last_page = False
-    product = Product.objects.order_by('-create_date')[first_product:last_product]
-    if Product.objects.order_by('-create_date')[last_product:last_product+1] == None :
+    if request.method == 'POST':
+        user_list = User.objects.filter(username__icontains=request.POST['seller_name'])
+        user_extend_data_list = UserExtendData.objects.filter(user__in=user_list)
+        if request.POST['sort'] == 'late' :
+            product = Product.objects.order_by('-create_date')
+        elif request.POST['sort'] == 'old' :
+            product = Product.objects.order_by('create_date')
+        elif request.POST['sort'] == 'low':
+            product = Product.objects.order_by('price')
+        else :
+            product = Product.objects.order_by('-price')
+        product = product.filter(seller__in=user_extend_data_list, name__icontains=request.POST['product_name'])[first_product:last_product+1]
+    else :
+        product = Product.objects.order_by('-create_date')[first_product:last_product+1]
+    if product[last_product:last_product+1] == None :
         is_last_page = True
+    product = product[first_product:last_product]
     context = {
         'type': '',
         'previous_page': page-1,
@@ -49,77 +75,77 @@ def catalog(request, num="1"):
     return render(request, template, context)
 
 def top(request, num="1"):
-    context = filter('Top', num)
+    context = filter(request, 'Top', num)
     if context == -1 :
         return redirect('/shop/top')
     template = 'catalog.html'
     return render(request, template, context)
 
 def jacket(request, num="1"):
-    context = filter('Jac', num)
+    context = filter(request, 'Jac', num)
     if context == -1 :
         return redirect('/shop/jacket')
     template = 'catalog.html'
     return render(request, template, context)
 
 def dress(request, num="1"):
-    context = filter('Dre', num)
+    context = filter(request, 'Dre', num)
     if context == -1 :
         return redirect('/shop/dress')
     template = 'catalog.html'
     return render(request, template, context)
 
 def skirt(request, num="1"):
-    context = filter('Ski', num)
+    context = filter(request, 'Ski', num)
     if context == -1 :
         return redirect('/shop/skirt')
     template = 'catalog.html'
     return render(request, template, context)
 
 def pants(request, num="1"):
-    context = filter('Pan', num)
+    context = filter(request, 'Pan', num)
     if context == -1 :
         return redirect('/shop/pants')
     template = 'catalog.html'
     return render(request, template, context)
 
 def shorts(request, num="1"):
-    context = filter('Sht', num)
+    context = filter(request, 'Sht', num)
     if context == -1 :
         return redirect('/shop/shorts')
     template = 'catalog.html'
     return render(request, template, context)
 
 def tshirt(request, num="1"):
-    context = filter('T-s', num)
+    context = filter(request, 'T-s', num)
     if context == -1 :
         return redirect('/shop/tshirt')
     template = 'catalog.html'
     return render(request, template, context)
 
 def suits(request, num="1"):
-    context = filter('Sui', num)
+    context = filter(request, 'Sui', num)
     if context == -1 :
         return redirect('/shop/suits')
     template = 'catalog.html'
     return render(request, template, context)
 
 def bag(request, num="1"):
-    context = filter('Bag', num)
+    context = filter(request, 'Bag', num)
     if context == -1 :
         return redirect('/shop/bag')
     template = 'catalog.html'
     return render(request, template, context)
 
 def shoes(request, num="1"):
-    context = filter('Sho', num)
+    context = filter(request, 'Sho', num)
     if context == -1 :
         return redirect('/shop/shoes')
     template = 'catalog.html'
     return render(request, template, context)
 
 def accessory(request, num="1"):
-    context = filter('Acc', num)
+    context = filter(request, 'Acc', num)
     if context == -1 :
         return redirect('/shop/accessory')
     template = 'catalog.html'
