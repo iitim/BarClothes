22bfd55c99a266d7b8b5568a503f5ffbe55a4a23
MMaetha <mod41744555@hotmail.com>
2017-10-26 17:01:21 +0700

    rearrange code in forms.py

--- src/main/forms.py
+++ src/main/forms.py
@@ -1,17 +1,6 @@
-# make sure this is at the top if it isn't already
 from django import forms
 
 class ContactForm(forms.Form):
-    contact_name = forms.CharField(required=True)
+    contact_name = forms.CharField(required=True, max_length=100, help_text='100 characters max.')
     contact_email = forms.EmailField(required=True)
-    content = forms.CharField(
-        required=True,
-        widget=forms.Textarea
-    )
-
-    def __init__(self, *args, **kwargs):
-        super(ContactForm, self).__init__(*args, **kwargs)
-        self.fields['contact_name'].label = "Your name:"
-        self.fields['contact_email'].label = "Your email:"
-        self.fields['content'].label =
-            "What do you want to say?"
+    content = forms.CharField(required=True, widget=forms.Textarea)
