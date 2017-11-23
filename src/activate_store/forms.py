f16d20c1a29b04e644f8ac29c0868de965b1fe95
Natnaree Asavaseri <bunditnoikiku@gmail.com>
2017-11-24 00:34:25 +0700

    price > 0

--- src/activate_store/forms.py
+++ src/activate_store/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm, TextInput
 
 from main.models import TopUp
@@ -5,4 +6,10 @@ from main.models import TopUp
 class upload_img_form(ModelForm):
 	class Meta:
 		model = TopUp
-		fields = ['slip_pic', 'price']
+		fields = ['slip_pic', 'price'] 
+
+	def __init__(self, *args, **kwargs):
+		super(upload_img_form, self).__init__(*args, **kwargs)
+		self.fields['price'] = forms.IntegerField(
+			min_value = 0
+		)
